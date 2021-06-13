83d172b00f9b521d7aad82e5142d90f05a206906
Zarema <zaremakayasova@gmail.com>
2021-06-13 15:21:28 -0400

    added typing effect with typewriter-effect package to Home component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <div className={classes.Container}>
                 <div className={classes.Text}>
                     <h2>Hello! I am Zarema.</h2>
-                    <p>I'm a junior web developer currently based in North Bay, Ontario. I specialize in building user friendly, responsive, and modern websites that are robust and fun.
+                    <p>I am a passionate self-taught junior web developer currently based in North Bay, Ontario. I specialize in building user-friendly, modern, and semantic websites that are highly intuitive and responsive.
                     </p>
                     <p>When I'm not coding, you can find me reading books, cooking, meditating, hiking and exploring nature with my husband.</p>
                 </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import classes from './Home.module.css';
+import Typewriter from 'typewriter-effect';
 import main from '../../assets/main.svg';
 import icon from '../../assets/icon.svg';
 
@@ -7,8 +8,19 @@ const Home = () => {
     return (
         <div className={classes.Home}>
             <div className={classes.Container}>
-                <h1 className={classes.Hello}>Hi! I am Zarema.</h1>
-                <h1 className={classes.Welcome}>JUNIOR WEB DEVELOPER</h1>
+                <h1 className={classes.Name}>Hi! I am Zarema.</h1>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString('<h1>JUNIOR WEB DEVELOPER</h1>')
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .typeString('<h1>JUNIOR WEB DEVELOPER</h1>')
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .typeString('<h1>JUNIOR WEB DEVELOPER</h1>')
+                            .start();
+                    }}
+                />
             </div>
             <img className={classes.Main} src={main} alt='main-img' />
             <img className={classes.Icon} src={icon} alt='icon-img' />
